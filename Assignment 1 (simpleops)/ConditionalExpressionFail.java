df37b4e317b5979b86d89a65b94ab4c0d10597a6
Sawyer Bowerman <117670150+shplok@users.noreply.github.com>
2025-01-31 14:10:04 -0500

    Initial Commit of Assignment 1

---
+++ Assignment 1 (simpleops)/ConditionalExpressionFail.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+
+public class ConditionalExpression {
+    public static void main(String[] args) {
+        System.out.println(42 ? true : false);
+        System.out.println(3 == 3 ? true : 42);
+        System.out.println(3 == 3 ? 42 : false);
+    }
+}
+
---
+++ Assignment 1 (simpleops)/DoStatementFail.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+
+public class DoStatement {
+    public static void main(String[] args) {
+        do {
+            System.out.println("Hello, World");
+        } while(42);
+    }
+}
+
